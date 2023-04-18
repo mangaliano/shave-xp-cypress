63ce1dd8872db570fae2fb165a4eb7fae749ff92
Amanda Alcantara Pinto Galiano <amanda.galiano@infoxdev.local>
2023-04-18 19:37:03 -0300

    Solicitando Serviços

---
+++ cypress/e2e/order.cy.js
@@ -0,0 +1,31 @@
+
+import loginPage from '../support/pages/login'
+import shaversPage from '../support/pages/shavers'
+import catalogPage from '../support/pages/catalog'
+import orderPage from '../support/pages/order'
+
+import data from '../fixtures/order.json'
+
+describe('pedido', () => {
+
+    context('usuario logado', () => {
+
+        const { customer, shaver, service } = data
+
+        before(() => {
+            cy.createUser(customer)
+            cy.apiLogin(customer)
+        })
+
+        it('deve poder solicitar serviços', () => {
+            shaversPage.selectShaver(shaver.name)
+            catalogPage.hasShaver(shaver.name)
+
+            catalogPage.selectService(service.description)
+            catalogPage.hasTitle(service.description)
+
+            catalogPage.confirmOrder()
+            orderPage.hasOrder()
+        })
+    })
+})
